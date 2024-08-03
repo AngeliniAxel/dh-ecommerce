c9c69cb153149372a1fcbc7a18134e892c687d30
AngeliniAxel <axel.angelini@gmail.com>
2024-08-03 14:20:56 +0200

    Added Products interface

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -6,16 +6,6 @@ import { CardProduct } from '../../components/ui/CardProduct/CardProduct';
 const Home = () => {
   const [products, setProducts] = useState([]);
 
-  const getProducts = async () => {
-    try {
-      const response = await fetch('http://localhost:3000/products');
-      const data = await response.json();
-      setProducts(data);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
   useEffect(() => {
     getProducts();
   }, []);
