4fad3408916c00d15c03b73f864c91f39a359d00
AngeliniAxel <axel.angelini@gmail.com>
2024-08-05 16:57:56 +0200

    Added Credit Card Form

--- src/pages/Checkout/Checkout.tsx
+++ src/pages/Checkout/Checkout.tsx
@@ -1,3 +1,4 @@
+import { CardCredit } from '../../components/ui/CardCredit/CardCredit';
 import { Table } from '../../components/ui/Table/Table';
 import styles from './Checkout.module.css';
 
@@ -6,11 +7,14 @@ const Checkout = () => {
     <div className={styles.container}>
       <h1 className={styles.title}>Checkout</h1>
       <div className={styles.grid}>
-        <div>
+        <div className={styles.tableContainer}>
           <Table />
         </div>
-        <div>{/* Formulario tarjeta */}</div>
+        <div>
+          <CardCredit />
+        </div>
       </div>
+      <button className={styles.buyButton}>Buy now</button>
     </div>
   );
 };
