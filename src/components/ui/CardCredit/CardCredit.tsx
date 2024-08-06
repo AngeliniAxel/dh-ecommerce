0fec04a90f146ec5b0b5260f653cbf7af14e7148
AngeliniAxel <axel.angelini@gmail.com>
2024-08-06 13:08:53 +0200

    Added Clear Cart

--- src/components/ui/CardCredit/CardCredit.tsx
+++ src/components/ui/CardCredit/CardCredit.tsx
@@ -3,6 +3,8 @@ import 'react-credit-cards-2/dist/es/styles-compiled.css';
 import styles from './CardCredit.module.css';
 import { useState } from 'react';
 import { toast } from 'sonner';
+import useCartContext from '../../../hooks/useCartContext';
+import { CartProduct } from '../../../interface';
 
 export const CardCredit = () => {
   const [cardData, setCardData] = useState({
@@ -12,6 +14,8 @@ export const CardCredit = () => {
     cvc: '',
     focus: '',
   });
+
+  const { dispatch } = useCartContext();
 
   const { number, name, expiry, cvc } = cardData;
 
@@ -47,6 +51,8 @@ export const CardCredit = () => {
       cvc: '',
       focus: '',
     });
+
+    dispatch({ type: 'CLEAR_CART', payload: {} as CartProduct });
   };
 
   return (
