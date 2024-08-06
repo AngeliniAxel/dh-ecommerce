391168e41f19f9ed8da304a9ea557672bb15ec9b
AngeliniAxel <axel.angelini@gmail.com>
2024-08-06 15:05:30 +0200

    Added pagination

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,31 +1,19 @@
-import { useEffect, useState } from 'react';
-import { Hero } from '../../components/ui/Hero';
-import styles from './Home.module.css';
-import { CardProduct } from '../../components/ui/CardProduct/CardProduct';
-import { getProducts } from '../../service';
-import { Product } from '../../interface';
-import { Toaster } from 'sonner';
 import { useQuery } from 'react-query';
+import { Toaster } from 'sonner';
+import { CardProduct } from '../../components/ui/CardProduct/CardProduct';
+import { Hero } from '../../components/ui/Hero';
+import { getProducts } from '../../service';
+import styles from './Home.module.css';
+import { useState } from 'react';
 
 const Home = () => {
-  const { data, isLoading, error } = useQuery('products', getProducts);
+  const [page, setPage] = useState(1);
 
-  /*  const [products, setProducts] = useState<Product[]>([]);
-  const [error, setError] = useState(false);
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    getProducts()
-      .then((data) => {
-        setProducts(data);
-      })
-      .catch(() => {
-        setError(true);
-      })
-      .finally(() => {
-        setIsLoading(false);
-      });
-  }, []); */
+  const { data, isLoading, error } = useQuery(
+    ['products', page],
+    () => getProducts(page),
+    { keepPreviousData: true }
+  );
 
   return (
     <>
@@ -33,10 +21,26 @@ const Home = () => {
       <Toaster richColors />
       {isLoading && <p>Loading...</p>}
       {error && <p>Something went wrong</p>}
-      <div className={styles.container}>
-        {data?.map((product) => (
-          <CardProduct key={product.tail} product={product} />
-        ))}
+      {
+        <div className={styles.container}>
+          {data?.map((product) => (
+            <CardProduct key={product.tail} product={product} />
+          ))}
+        </div>
+      }
+      <div>
+        <button onClick={() => setPage(page - 1)} disabled={page === 1}>
+          previous page
+        </button>
+        <div>
+          <span>{page}</span>
+        </div>
+        <button
+          onClick={() => setPage(page + 1)}
+          disabled={data?.length === page}
+        >
+          next page
+        </button>
       </div>
     </>
   );
--- src/service/products.service.ts
+++ src/service/products.service.ts
@@ -1,12 +1,15 @@
 import { Product } from '../interface';
 
-export const getProducts = async (): Promise<Product[]> => {
+export const getProducts = async (page = 0): Promise<Product[]> => {
   try {
-    const response = await fetch('http://localhost:3000/products');
+    const response = await fetch(
+      `http://localhost:3000/products?_page=${page}&_per_page=24`
+    );
 
     if (response.ok) {
       const data = await response.json();
-      return data;
+      console.log(data);
+      return data.data;
     } else {
       throw new Error('Failed to fetch products');
     }
